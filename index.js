5fba596b253926de27dad1fb42bde342109ccf4a
Жеребко Дмитрий < >
2016-06-20 23:57:41 +0300

    kek

--- index.js
+++ index.js
@@ -3,17 +3,9 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-
-app.get('/', function(request, response) {
-  response.render('pages/index.html');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic("./views/pages").listen(8080, function(){
+  
+}));
 
