0ca7ffa5e3b2c6f23e83d291abb613a0d2163ec2
Жеребко Дмитрий < >
2016-06-21 00:30:13 +0300

    fix dirs

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('views/pages'));
+app.use(express.static('public'));
 
 app.set('port', (process.env.PORT || 5000));
 app.get('/', function (req, res) {
-  res.sendFile(__dirname+"/views/pages/index.html")
+  res.sendFile(__dirname+"/public/index.html")
 });
 
 app.listen(app.get('port'), function() {
